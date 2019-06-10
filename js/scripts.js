b2cb57425845273811691eb3b09b456c3f7adf00
Matt Eilar <meilar@gmail.com>
2019-06-09 18:50:04 -0700

    finish assembling js events

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,26 @@ $(document).ready(function() {
   $("form#assessment").submit(function(event) {
     if (bathroom1.checked && friends1.checked) {
       $("#a11").show();
-      alert("condition yes");
+    } else if (bathroom1.checked && friends2.checked) {
+      $("#a21").show();
+    } else if (bathroom1.checked && friends3.checked) {
+      $("#a31").show();
+    } else if (bathroom2.checked && friends1.checked) {
+      $("#a12").show();
+    } else if (bathroom2.checked && friends2.checked) {
+      $("#a22").show();
+    } else if (bathroom2.checked && friends3.checked) {
+      $("#a32").show();
+    } else if (bathroom3.checked && friends1.checked) {
+      $("#a13").show();
+    } else if (bathroom3.checked && friends2.checked) {
+      $("#a23").show();
+    } else if (bathroom3.checked && friends3.checked) {
+      $("#a33").show();
     } else {
-      alert("empty")
-    }
+
+    };
+    alert("after elses");
     $(".main-body").hide();
     $("#again").show();
     event.preventDefault();
